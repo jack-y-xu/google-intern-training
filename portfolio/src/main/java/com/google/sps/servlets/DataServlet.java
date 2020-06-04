57369af075dfc64a3cc93a84e0600bf5f43f950a
Jack Xu <xujack2001@gmail.com>
2020-06-04 18:25:20 +0000

    cosmetic upgrades and implemented datastore for storing comments.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -13,9 +13,10 @@
 // limitations under the License.
 
 import java.util.*;
+import com.google.appengine.api.datastore.*;
+import com.google.appengine.api.datastore.Query.*;
 import com.google.gson.Gson;
 import java.io.IOException;
-import java.util.Date;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,14 +25,19 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that handles comments from the user and returns a list of current comments **/
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-  private ArrayList<String> comments = new ArrayList<String>();
   private static final String BUTTON_PARAMETER = "button";
   private static final String COMMENT_BOX_PARAMETER = "comment-box";
   private static final String CLEAR_BUTTON_VALUE = "clear-all-comments";
   private static final String SUBMIT_BUTTON_VALUE = "submit";
-
+  private DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Query query = new Query("Comment").addSort("timestamp",SortDirection.DESCENDING);
+    PreparedQuery results = datastore.prepare(query);
+    ArrayList<String> comments = new ArrayList<String>();
+    for(Entity entity:results.asIterable()) {
+      comments.add((String)entity.getProperty("content"));
+    }
     String json = convertToJson(comments);
     response.setContentType("application/json");
     response.getWriter().println(json);
@@ -48,14 +54,23 @@ public class DataServlet extends HttpServlet {
     String choice = request.getParameter(BUTTON_PARAMETER);
     if (choice.equals(SUBMIT_BUTTON_VALUE)) {
       String content = request.getParameter(COMMENT_BOX_PARAMETER);
-      if (content.length() > 0) comments.add(content);
-      else {
+      if (content.length() > 0) {
+        long timestamp = System.currentTimeMillis();
+        Entity taskEntity = new Entity("Comment");
+        taskEntity.setProperty("content", content);
+        taskEntity.setProperty("timestamp", timestamp);
+        datastore.put(taskEntity);
+      } else {
         response.setContentType("text/html");
         response.getWriter().println("Please enter a non-empty comment.");
         return;
       }
     } else if (choice.equals(CLEAR_BUTTON_VALUE)) {
-      comments.clear();
+        Query mydeleteq = new Query();
+        PreparedQuery pq = datastore.prepare(mydeleteq);
+        for (Entity result : pq.asIterable()) {
+          datastore.delete(result.getKey());      
+        } 
     }
     response.sendRedirect("/index.html");
   }
